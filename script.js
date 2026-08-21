041183823ef67c6556c30730510d701d5969915a
blazingkgn <emps45@gmail.com>
2026-08-21 22:39:59 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,92 @@
+setTimeout(function(){
+
+    const loader = document.querySelector(".loader");
+
+
+    loader.classList.add("hide");
+
+
+    loader.style.opacity="0";
+
+
+    setTimeout(function(){
+
+        loader.style.display="none";
+
+    },1200);
+
+
+},3000);
+
+const music = document.getElementById("bgMusic");
+const musicBtn = document.getElementById("musicBtn");
+
+let isPlaying = false;
+
+musicBtn.addEventListener("click", function () {
+
+    if (isPlaying) {
+
+        music.pause();
+        musicBtn.innerHTML = "♫";
+
+    } else {
+
+       music.play().catch(error => console.log(error));
+        musicBtn.innerHTML = "❚❚";
+
+    }
+
+    isPlaying = !isPlaying;
+
+});
+/*==============================
+      ОТЗЫВЫ
+==============================*/
+
+const reviewBtn = document.getElementById("reviewBtn");
+const authModal = document.getElementById("authModal");
+const closeModal = document.getElementById("closeModal");
+const loginBtn = document.getElementById("loginBtn");
+
+if (reviewBtn && authModal) {
+
+    reviewBtn.addEventListener("click", function () {
+
+        authModal.style.display = "flex";
+
+    });
+
+}
+
+if (closeModal) {
+
+    closeModal.addEventListener("click", function () {
+
+        authModal.style.display = "none";
+
+    });
+
+}
+
+if (loginBtn) {
+
+    loginBtn.addEventListener("click", function () {
+
+        alert("Для продолжения необходимо авторизоваться.");
+
+        authModal.style.display = "none";
+
+    });
+
+}
+
+window.addEventListener("click", function (e) {
+
+    if (e.target === authModal) {
+
+        authModal.style.display = "none";
+
+    }
+
+});
